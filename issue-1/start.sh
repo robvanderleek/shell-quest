81183bc02451a96109759a6ac16024da42f6b947
Rob van der Leek <robvanderleek@gmail.com>
2021-09-21 23:10:19 +0200

    Add curl, set password

--- issue-1/start.sh
+++ issue-1/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-if [ "$1" = "ttyd" ]; then
-    ttyd.x86_64 -t "titleFixed=Shell Quest" -t rendererType=webgl \
-    /bin/bash -i -l
-elif [ -t 0 ]; then
+if [ -t 0 ]; then
     /bin/bash
 else
     IMAGE="robvanderleek/sq-i1"
